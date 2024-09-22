9d33953e8637f77c88d5ad39824832d271d136b8
codeitnav <navya.sriv.1110@gmail.com>
2024-09-22 12:35:03 +0530

    update file 2

---
+++ 02simpleStorage_arrays_structs.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26; //solidity version 
+
+contract SimpleStorage {
+
+    uint256 public myFavNum = 88; 
+
+    function store(uint256 _favNum) public {
+        myFavNum = _favNum;
+        retrieve();
+    }
+
+    function retrieve() public view returns(uint256){
+        return myFavNum;
+    }
+
+    // array : uint256[] listOfFavNum;
+
+    struct Person{
+        uint256 favNum;
+        string name;
+    }
+
+    // method 1 : Person public myFriend = Person(5, "Nav");
+    // method 2 :
+    Person public myFriend = Person({favNum: 5, name: "Nav"});
+
+    // dynamic array - size can vary
+    Person[] public listofPeople;
+
+    function addPerson(string memory _name, uint256 _favNum) public {
+        _name = "cat";
+        listofPeople.push(Person({favNum: _favNum, name: _name}));
+    }
+    // calldata, memory : the variable (_name here) will exist temporarily, during the duration of function call
+    // e.g.: if we called addPerson and gave name "patrick" , it'll store once and we can never call "patrick" again
+    // calldata - temporary variable that cannot be modified
+    /* e.g.: function addPerson(string calldata _name, uint256 _favNum) public {
+        _name = "cat"; 
+        listofPeople.push(Person({favNum: _favNum, name: _name}));
+    } ----> INVALID
+    
+    */
+
+}
